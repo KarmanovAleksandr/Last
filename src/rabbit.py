d2f55a776bfb76582541ecf97f8af74abe62940f
KarmanovAleksandr <138717442+KarmanovAleksandr@users.noreply.github.com>
2023-07-21 16:16:12 +0300

    Update rabbit.py

--- src/rabbit.py
+++ src/rabbit.py
@@ -1,5 +1,9 @@
 import pika
+
 from .settings import settings
+
+message = list()
+
 
 def add_message(text):
     connection = pika.BlockingConnection(pika.ConnectionParameters(settings.rabbit_host))
@@ -8,11 +12,13 @@ def add_message(text):
     channel.basic_publish(exchange='', routing_key='test', body=text)
     connection.close()
 
+
 def read_message():
     connection = pika.BlockingConnection(pika.ConnectionParameters(settings.rabbit_host))
     channel = connection.channel()
     channel.queue_declare(queue='test')
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
+        message.append(body)
     channel.basic_consume(queue='test',on_message_callback=callback, auto_ack=True)
     channel.start_consuming()
